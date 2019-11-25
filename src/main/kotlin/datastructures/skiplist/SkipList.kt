dc82d84a7844e54bca244804e436445024f47536
sh <vshushkanov@berlogic.de>
2019-11-25 11:01:14 +0100

    head node

--- src/main/kotlin/datastructures/skiplist/SkipList.kt
+++ src/main/kotlin/datastructures/skiplist/SkipList.kt
@@ -7,22 +7,24 @@ const val MAX_HEIGHT = 10
 /**
  * @see java.util.concurrent.ConcurrentSkipListSet
  */
-class Node (var value: Int, var forward: Array<Node?>) {
-    constructor(value: Int) : this(value, Array(MAX_HEIGHT, { null }))
+class Node (var value: Int?, var forward: Array<Node?>) {
+    constructor(value: Int?) : this(value, Array(MAX_HEIGHT, { null }))
 
     override fun toString(): String {
-        return value.toString()
+        return if (value == null) "x" else value.toString()
     }
 
 }
 
 class SkipList(var head: Node) {
-    constructor() : this(Node(Int.MIN_VALUE))
+    constructor() : this(Node(null))
 
     fun scan(value: Int, links : Array<Node?>? = null): Node {
         var current : Node = head
         for (level in MAX_HEIGHT - 1 downTo 0) {
-            while (current.forward[level] != null && current.forward[level]!!.value <= value) {
+            while (current.forward[level] != null
+                && (current.forward[level]!!.value == null
+                || current.forward[level]!!.value!! <= value)) {
                 current = current.forward[level]!!
             }
             links?.let {
@@ -65,7 +67,7 @@ class SkipList(var head: Node) {
             result.append("$node -> ")
             while (node != null) {
                 val next = node.forward[level]
-                result.append(if (next != null) "$next -> " else "null")
+                result.append(if (next != null) "$next -> " else "x")
                 node = next
             }
             result.append("\n")
